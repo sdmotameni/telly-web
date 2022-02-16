82077c013147835366ebfe68d47f47eb40bb764d
Sep Motameni <sepmotameni@Seps-MacBook-Pro.local>
2022-02-16 17:25:50 -0500

    Added SSR

--- pages/[id].js
+++ pages/[id].js
@@ -1,42 +1,65 @@
-import { Component } from "react";
+import { useEffect } from "react";
 
 import ProfileService from "../services/profileService";
 
-import Loading from "../components/loading";
 import Profile from "../components/profile";
 
-export default class Id extends Component {
-  state = { component: <Loading /> };
+export async function getServerSideProps({ params, query }) {
+  const { id } = params;
 
-  componentDidMount() {
-    const id = window.location.pathname.substring(1);
+  const { data } = await ProfileService.getProfile(id);
 
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    const params = Object.fromEntries(urlSearchParams.entries());
-
-    ProfileService.getProfile(id)
-      .then(({ data }) => {
-        this.setState({
-          component: <Profile data={data} header={!params["h"]} />,
-        });
-      })
-      .catch(({ _, serversDown }) => {
-        if (!serversDown) {
-          this.setState({
-            component: (
-              <div className="mt-10 text-sm font-light text-center text-gray-700 opacity-70">
-                No Telly profile found at this ID. Contact support@gettelly.com
-                if you believe this is an error.
-              </div>
-            ),
-          });
-        } else {
-          window.location = "/maintenance";
-        }
-      });
-  }
-
-  render() {
-    return this.state.component;
-  }
+  return {
+    props: {
+      data,
+      query,
+    },
+  };
 }
+
+export default function ID({ data, query }) {
+  const NoProfileComponent = (
+    <div className="mt-10 text-sm font-light text-center text-gray-700 opacity-70">
+      No Telly profile found at this ID. Contact support@gettelly.com if you
+      believe this is an error.
+    </div>
+  );
+
+  return data ? <Profile data={data} header={!query["h"]} /> : <div>hi</div>;
+}
+
+// export default class Id extends Component {
+//   state = { component: <Loading /> };
+
+//   componentDidMount() {
+//     const id = window.location.pathname.substring(1);
+
+//     const urlSearchParams = new URLSearchParams(window.location.search);
+//     const params = Object.fromEntries(urlSearchParams.entries());
+
+//     ProfileService.getProfile(id)
+//       .then(({ data }) => {
+//         this.setState({
+//           component: <Profile data={data} header={!params["h"]} />,
+//         });
+//       })
+//       .catch(({ _, serversDown }) => {
+//         if (!serversDown) {
+// this.setState({
+//   component: (
+//     <div className="mt-10 text-sm font-light text-center text-gray-700 opacity-70">
+//       No Telly profile found at this ID. Contact support@gettelly.com
+//       if you believe this is an error.
+//     </div>
+//             ),
+//           });
+//         } else {
+//           window.location = "/maintenance";
+//         }
+//       });
+//   }
+
+//   render() {
+//     return this.state.component;
+//   }
+// }
